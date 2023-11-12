a675f4c49d2d814208cdd8faae1a6ac7718808b5
Olatunji Oluwadare <tunjidare2@yahoo.com>
2023-11-11 21:48:35 -0800

    The customized printf project

---
+++ _printf.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - Customized printf function
+ * @format: Format string
+ * Olatunji Oluwadare and Akpan Goodnews
+ * Return: Number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+int i, printed = 0, printed_chars = 0;
+int flags, width, precision, size, buff_ind = 0;
+va_list a_list;
+char buffer[BUFF_SIZE];
+
+if (format == NULL)
+return (-1);
+
+va_start(a_list, format);
+
+for (i = 0; format && format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+
+/* write(1, &format[i], 1);*/
+printed_chars++;
+}
+else
+{
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, a_list);
+precision = get_precision(format, &i, a_list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, a_list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
+}
+}
+
+print_buffer(buffer, &buff_ind);
+
+va_end(a_list);
+
+return (printed_chars);
+}
+
+/**
+ * print_buffer - Outputs the contents of the buffer, if it is not empty
+ * @buffer: Array of characters to be displayed
+ * @buff_ind: Index indicating the length of the buffer
+ */
+
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+
+*buff_ind = 0;
+}
---
+++ functions2.c
@@ -0,0 +1,202 @@
+#include "main.h"
+
+/****************** POINTER PRINTER ******************/
+
+/**
+ * print_pointer - Prints the value of a pointer variable
+ * @types: Arguments list
+ * Olatunji Oluwadare and Goodnews Akpan
+ * @buffer: Buffer array
+ * @flags: Active flags for formatting
+ * @width: Width specification
+ * @precision: Specification
+ * @size: Specifier
+ * Return: Num of characters printed
+ */
+
+
+int print_pointer(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+char extra_c = 0, padd = ' ';
+int ind = BUFF_SIZE - 2, length = 2, padd_start = 1;
+unsigned long num_addrs;
+char map_to[] = "0123456789abcdef";
+void *addrs = va_arg(types, void *);
+
+UNUSED(width);
+UNUSED(size);
+
+if (addrs == NULL)
+return (write(1, "(nil)", 5));
+
+buffer[BUFF_SIZE - 1] = '\0';
+UNUSED(precision);
+
+num_addrs = (unsigned long)addrs;
+
+while (num_addrs > 0)
+{
+buffer[ind--] = map_to[num_addrs % 16];
+num_addrs /= 16;
+length++;
+}
+
+if ((flags & F_ZERO) && !(flags & F_MINUS))
+padd = '0';
+if (flags & F_PLUS)
+extra_c = '+', length++;
+else if (flags & F_SPACE)
+extra_c = ' ', length++;
+
+ind++;
+
+return (write_pointer(buffer, ind, length,
+width, flags, padd, extra_c, padd_start));
+}
+
+/************************* PRINT NON-PRINTABLE *************************/
+
+/**
+ * print_non_printable - Prin ASCII codes in hex format/non-printable char
+ * @types: List of arguments
+ * @buffer: Array for printing
+ * @flags: Active flags for formatting
+ * @width: Width specification
+ * Olatunji Oluwadare and Goodnews Akpan
+ * @precision: Specification
+ * @size: Specifier
+ * Return: Number of characters printed
+ */
+
+int print_non_printable(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+int i = 0, offset = 0;
+char *str = va_arg(types, char *);
+
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+
+if (str == NULL)
+return (write(1, "(null)", 6));
+
+while (str[i] != '\0')
+{
+if (is_printable(str[i]))
+buffer[i + offset] = str[i];
+else
+offset += append_hexa_code(str[i], buffer, i + offset);
+
+i++;
+}
+
+buffer[i + offset] = '\0';
+
+return (write(1, buffer, i + offset));
+}
+
+/************************* PRINT IN REVERSE *************************/
+
+/**
+ * print_reverse - Prints a reversed string
+ * @types: Lists arguments
+ * @buffer: Array for printing
+ * @flags: Active flags for formatting
+ * @width: Specification
+ * @precision: Specification
+ * Goodnews Akpan
+ * @size: Specifier
+ * Return: Number of characters to be printed
+ */
+
+
+
+int print_reverse(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+char *str;
+int i, count = 0;
+
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(size);
+
+str = va_arg(types, char *);
+
+if (str == NULL)
+{
+UNUSED(precision);
+
+str = ")Null(";
+}
+for (i = 0; str[i]; i++)
+;
+
+for (i = i - 1; i >= 0; i--)
+{
+char z = str[i];
+
+write(1, &z, 1);
+count++;
+}
+return (count);
+}
+
+/************************* PRINT STRING IN ROT13 *************************/
+
+/**
+ * print_rot13string - Prints a string encoded in ROT13
+ * @types: Lis arguments
+ * @buffer: Array for printing
+ * @flags: For formatting
+ * @width: Width specification
+ * @precision: Precision specification
+ * Olatunji Oluwadare
+ * @size: Specifier
+ * Return: Number of characters
+ */
+
+int print_rot13string(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+char x;
+char *str;
+unsigned int i, j;
+int count = 0;
+char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+str = va_arg(types, char *);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+
+if (str == NULL)
+str = "(AHYY)";
+for (i = 0; str[i]; i++)
+{
+for (j = 0; in[j]; j++)
+{
+if (in[j] == str[i])
+{
+x = out[j];
+write(1, &x, 1);
+count++;
+break;
+			}
+}
+if (!in[j])
+{
+x = str[i];
+write(1, &x, 1);
+count++;
+}
+}
+return (count);
+}
